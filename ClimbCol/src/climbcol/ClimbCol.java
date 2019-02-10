a75a2db1a9059f5d46d25e6e16dcf95ae88fa45c
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-02-09 20:36:29 -0500

    Update ClimbCol.java

--- ClimbCol/src/climbcol/ClimbCol.java
+++ ClimbCol/src/climbcol/ClimbCol.java
@@ -16,7 +16,7 @@ public class ClimbCol {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("fdsf");
+        System.out.println("hola jaja por fin lo logre ");
     }
     
 }
