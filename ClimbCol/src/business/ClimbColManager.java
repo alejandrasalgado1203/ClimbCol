c79d1e0f159d077da3768d27ee40d7122f905dab
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-02-27 07:38:57 -0500

    Update ClimbColManager.java

--- ClimbCol/src/business/ClimbColManager.java
+++ ClimbCol/src/business/ClimbColManager.java
@@ -7,6 +7,7 @@ import java.util.TreeSet;
 import data.*;
 
 public class ClimbColManager {
+
 	private static TreeMap<String,Parque> parks;
 
 
@@ -24,25 +25,17 @@ public class ClimbColManager {
 		return parks.keySet().toArray();
 	}
 
-
-
 	public static Zona getZone(Parque p, String name) {
 		return p.getZona(name);
 	}
-
-
 
 	public static Parque getPark(String name) {
 		return parks.get(name);
 	}
 
-
-
 	public static Object[] getZonesNames(Parque p) {
 		return p.getZonas().keySet().toArray();
 	}
-
-
 
 	public static TreeMap<String,Parque> getParks() {
 		return parks;
@@ -50,6 +43,14 @@ public class ClimbColManager {
 
 	public static void setParks(TreeMap<String,Parque> parks) {
 		ClimbColManager.parks = parks;
+	}
+
+	public static Object[] getRutesNames(Zona zone) {
+		return zone.getRutas().keySet().toArray();
+	}
+
+	public static Ruta getRute(Zona zone, String selectedValue) {
+		return zone.getRuta(selectedValue);
 	}
 
 }
