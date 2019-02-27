5736d7e9e9ae16704cec1f738826c85aae6fb62f
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-02-27 07:41:50 -0500

    Update UIZone.java

--- ClimbCol/src/ui/UIZone.java
+++ ClimbCol/src/ui/UIZone.java
@@ -22,16 +22,17 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import business.ClimbColManager;
+import data.Zona;
 
 public class UIZone extends JPanel {
-	private static JPanel panelRutes = new JPanel ();
-    private static JPanel centerPanel = new JPanel();
-    private ClimbColManager climbcolManager;
-    private UIWelcome welcome;
+	private static UIRute panelRutes;
+	private static JPanel centerPanel = new JPanel();
+	private UIWelcome welcome;
+	private Zona zone;
 
-	public UIZone(Object zone) {
+	public UIZone(Zona zone) {
+		this.zone = zone;
 		showPanelZone();
-		this.climbcolManager = climbcolManager;
 	}
 
 	public void setupMainPanel(String nameZone) {
@@ -39,48 +40,48 @@ public class UIZone extends JPanel {
 		createScrollPane();
 		createDescription();
 	}
-	
+
 	public void createTittle(String nameZone) {
 		JPanel tittle = new JPanel();
 		JLabel lblWelcomeZone = new JLabel("ZONA "+ nameZone);
-        lblWelcomeZone.setFont(new Font("Tahoma",Font.PLAIN,35));
-        tittle.add(lblWelcomeZone);
-        this.add(tittle,BorderLayout.NORTH);
+		lblWelcomeZone.setFont(new Font("Tahoma",Font.PLAIN,35));
+		tittle.add(lblWelcomeZone);
+		this.add(tittle,BorderLayout.NORTH);
 	}
-	
+
 	public static void createDescription() {
-    	JPanel description = new JPanel();
-    	description.setLayout(new GridLayout(2,1));
-    	centerPanel.add(description,new FlowLayout());
-    	
-    	ImageIcon Zone= new ImageIcon("images\\4.jpg");
-        JLabel labelImage = new JLabel(Zone);
+		JPanel description = new JPanel();
+		description.setLayout(new GridLayout(2,1));
+		centerPanel.add(description,new FlowLayout());
+
+		ImageIcon Zone= new ImageIcon("images\\4.jpg");
+		JLabel labelImage = new JLabel(Zone);
 		labelImage.setVisible(true);
 		description.add(labelImage);
-        
+
 		JTextField textDescription = new JTextField(30);
 		description.add(textDescription);
-    }
-	
-public void createScrollPane() {
-    	
-	JList listRutes = new JList(climbcolManager.getZonesNames());
-	JScrollPane scrollPaneRutes = new JScrollPane(listRutes);
-	
-	centerPanel.add(scrollPaneRutes,new FlowLayout());
-	this.add(centerPanel,BorderLayout.CENTER);
-    
-    listRutes.addListSelectionListener(new ListSelectionListener() {
-		public void valueChanged(ListSelectionEvent arg0) {
-			panelRutes = new UIZone(climbcolManager.getZone(arg0.getSource()));
-			showPanelZone();
-		}
-    });
-    }
-    
-    public void showPanelZone() {
-        this.setSize(900,900);
-        this.setVisible(true);
-        setupMainPanel(" ");
-    }
+	}
+
+	public void createScrollPane() {
+
+		JList listRutes = new JList(ClimbColManager.getRutesNames(zone));
+		JScrollPane scrollPaneRutes = new JScrollPane(listRutes);
+
+		centerPanel.add(scrollPaneRutes,new FlowLayout());
+		this.add(centerPanel,BorderLayout.CENTER);
+
+		listRutes.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent arg0) {
+				panelRutes = new UIRute(ClimbColManager.getRute(zone,(String) listRutes.getSelectedValue()));
+				showPanelZone();
+			}
+		});
+	}
+
+	public void showPanelZone() {
+		this.setSize(900,900);
+		this.setVisible(true);
+		setupMainPanel(" ");
+	}
 }
