bd67828c8ee5e87fa444d6a1e2b706ef5801d149
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-02-27 07:43:50 -0500

    Update UIRute.java

--- ClimbCol/src/ui/UIRute.java
+++ ClimbCol/src/ui/UIRute.java
@@ -18,38 +18,44 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class UIRute{
+import data.Ruta;
+
+public class UIRute extends JPanel{
 	private static JPanel panelRute= new JPanel();
 	private static JPanel centerPanel = new JPanel();
 	private static JFrame frame = new JFrame ("CLIMBCOL");
-	
-    public static void setupMainPanel(String nameRute) {
-    	createTittle(nameRute);
-    	createImage();
-    	createTextField();
-    	createMenuBar();
-    }
-    public static void createTittle(String nameRute) {
-    	
+	private Ruta rute;
+
+	public UIRute(Ruta rute) {
+		this.rute = rute;
+	}
+	public static void setupMainPanel(String nameRute) {
+		createTittle(nameRute);
+		createImage();
+		createTextField();
+		createMenuBar();
+	}
+	public static void createTittle(String nameRute) {
+
 		JLabel lblWelcomeRute = new JLabel("RUTA "+ nameRute);
-        lblWelcomeRute.setFont(new Font("Tahoma",Font.PLAIN,35));
-        frame.add(lblWelcomeRute,BorderLayout.NORTH);
-    }
-    public static void createImage() {
-        ImageIcon Rute= new ImageIcon("images\\2.jpg");
-        JLabel labelImage = new JLabel(Rute);
+		lblWelcomeRute.setFont(new Font("Tahoma",Font.PLAIN,35));
+		frame.add(lblWelcomeRute,BorderLayout.NORTH);
+	}
+	public static void createImage() {
+		ImageIcon Rute= new ImageIcon("images\\2.jpg");
+		JLabel labelImage = new JLabel(Rute);
 		labelImage.setVisible(true);
 		centerPanel.add(labelImage,new FlowLayout());
-    }
-    public static void createTextField() {
-    	JTextField textDescription = new JTextField(30);
+	}
+	public static void createTextField() {
+		JTextField textDescription = new JTextField(30);
 		centerPanel.add(textDescription,new FlowLayout());
 		frame.add(centerPanel,BorderLayout.CENTER);
-    }
-    public static void createMenuBar() {
+	}
+	public static void createMenuBar() {
 		JMenuBar menuBar = new JMenuBar();
 		panelRute.add(menuBar);
-		
+
 		JMenu menuAdd = new JMenu("Agregar");
 		menuAdd.setMnemonic(KeyEvent.VK_A);
 		menuBar.add(menuAdd);
@@ -64,26 +70,26 @@ public class UIRute{
 		menuItemFavorites.setMnemonic(KeyEvent.VK_B);
 		menuAdd.add(menuItemFavorites);
 		menuAdd.addSeparator();
-		
+
 		menuItemGoals.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-            	UIUser.showPanelGoals();
-            }
-        });
-        
-        menuItemFavorites.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-            	UIUser.showPanelFavorites();
-            }
-        });
-    }
-    public static void showPanelRute() {
-    	frame.setTitle("CLIMBCOL");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900,900);
-        frame.setResizable(true);
-        setupMainPanel(" ");
-        frame.pack();
-        frame.setVisible(true);
-    }
+			public void actionPerformed(ActionEvent e) {
+				UIUser.showPanelGoals();
+			}
+		});
+
+		menuItemFavorites.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				UIUser.showPanelFavorites();
+			}
+		});
+	}
+	public static void showPanelRute() {
+		frame.setTitle("CLIMBCOL");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(900,900);
+		frame.setResizable(true);
+		setupMainPanel(" ");
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
