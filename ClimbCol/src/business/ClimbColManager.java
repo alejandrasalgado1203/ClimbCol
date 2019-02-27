fb75c969a79bd8d0c7333364868ac1e7cbde3e32
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-02-27 07:01:10 -0500

    Update ClimbColManager.java

--- ClimbCol/src/business/ClimbColManager.java
+++ ClimbCol/src/business/ClimbColManager.java
@@ -9,44 +9,49 @@ import javax.swing.ListModel;
 import data.*;
 
 public class ClimbColManager {
-	private TreeMap<String,Parque> parks;
+	
+	private static TreeMap<String,Parque> parks;
 
-	public ClimbColManager(TreeMap<String, Parque> parks) {
-		super();
-		this.parks = parks;
-	}
-
-
-
-	public ArrayList<String> getAllRutes() {
+	
+	public static ArrayList<String> getAllRutes() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Object[] getParksNames() {
+	public static Object[] getParksNames() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
 
-	public Object getZone(Object source) {
+	public static Object getZone(Object source) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
 
-	public LayoutManager getPark(Object source) {
+	public static LayoutManager getPark(Object source) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
 
-	public ListModel getZonesNames() {
+	public static ListModel getZonesNames() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	
+	
+	public static TreeMap<String,Parque> getParks() {
+		return parks;
+	}
+
+	public static void setParks(TreeMap<String,Parque> parks) {
+		ClimbColManager.parks = parks;
 	}
 
 }
