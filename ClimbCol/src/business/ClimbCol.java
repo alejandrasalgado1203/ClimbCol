a0b0e3412ec68761de8fb9975dec75327733b361
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-02-27 07:02:14 -0500

    Update ClimbCol.java

--- ClimbCol/src/business/ClimbCol.java
+++ ClimbCol/src/business/ClimbCol.java
@@ -10,14 +10,19 @@ public class ClimbCol {
 	private static UIWelcome ui;
 	private static TreeMap <String,Parque> parques = new TreeMap <String,Parque>();
 	private static TreeMap <String,Escalador> escaladores = new TreeMap <String,Escalador>();
-    private static ClimbersManager climbersManager= new ClimbersManager();
-    private static ClimbColManager climbcolManager = new ClimbColManager(parques);
-    
+
 	public static void main(String[] args) {
-		
-		UIWelcome ui = new UIWelcome(climbersManager,climbcolManager);
+
+		initiComponents();
+
+		ui = new UIWelcome();
 		ui.showAll();
 		//UICreardorDeDatos cr = new UICreardorDeDatos(new CreadorDeDatos(parques));
+	}
+
+	private static void initiComponents() {
+		ClimbColManager.setParks(parques);
+		ClimbersManager.setEscaladores(escaladores);
 	}
 
 
