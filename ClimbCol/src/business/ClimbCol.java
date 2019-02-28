394fa38fe2dbdfacd07ede3f1a7cfe60d5031e9c
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-02-28 16:19:37 -0500

    Update ClimbCol.java

--- ClimbCol/src/business/ClimbCol.java
+++ ClimbCol/src/business/ClimbCol.java
@@ -7,7 +7,7 @@ import data.*;
 import ui.*;
 
 public class ClimbCol {
-	private static UIWelcome ui;
+	private static UIMain uiMain;
 	private static TreeMap <String,Parque> parques = new TreeMap <String,Parque>();
 	private static TreeMap <String,Escalador> escaladores = new TreeMap <String,Escalador>();
 
@@ -16,16 +16,16 @@ public class ClimbCol {
 		initiComponents();
 		parques.put("parque 1", new Parque("parque 1","images/4.jpg"));
 		parques.put("parque 2", new Parque("parque 2","images/sandPile3.png"));
-		parques.put("parque 3", new Parque("parque 3","images/sandPile3.png"));
+		parques.put("parque 3", new Parque("parque 3","images/4.jpg"));
 		parques.put("parque 4", new Parque("parque 4","images/sandPile3.png"));
-		parques.put("parque 5", new Parque("parque 5","images/sandPile3.png"));
+		parques.put("parque 5", new Parque("parque 5","images/4.jpg"));
 		parques.put("parque 6", new Parque("parque 6","images/sandPile3.png"));
-		parques.put("parque 7", new Parque("parque 7","images/sandPile3.png"));
+		parques.put("parque 7", new Parque("parque 7","images/4.jpg"));
 		parques.put("parque 8", new Parque("parque 8","images/sandPile3.png"));
-		parques.put("parque 9", new Parque("parque 9","images/sandPile3.png"));
+		parques.put("parque 9", new Parque("parque 9","images/4.jpg"));
 		
-		ui = new UIWelcome();
-		ui.showAll();
+		uiMain = new UIMain();
+		
 		//UICreardorDeDatos cr = new UICreardorDeDatos(new CreadorDeDatos(parques));
 	}
 
