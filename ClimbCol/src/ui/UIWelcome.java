0c140a2e8ef2e5e586282d53e38cf5af5ac108b9
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-03-01 17:38:54 -0500

    Update UIWelcome.java

--- ClimbCol/src/ui/UIWelcome.java
+++ ClimbCol/src/ui/UIWelcome.java
@@ -1,13 +1,22 @@
 
 package ui;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.Box;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -17,40 +26,31 @@ import javax.swing.event.ListSelectionListener;
 
 import business.ClimbColManager;
 import data.Parque;
+import data.Ruta;
 
 public class UIWelcome extends JPanel{
-	private Box mainBox;
-	private Box centerBox;
 	private UIMain uiMain;
+	private GridBagConstraints constraints = new GridBagConstraints();
 
 	public static UIWelcome createUIWelcome(UIMain main){
 		return new UIWelcome(main);
 	}
 
 	public UIWelcome(UIMain main){
-		super();
+		super(new GridBagLayout());
 		this.uiMain = main;
-		this.mainBox = Box.createVerticalBox();
-		this.mainBox.add(Box.createVerticalStrut(10));
-		this.centerBox = Box.createHorizontalBox();
-		this.centerBox.add(Box.createHorizontalStrut(40));
-		setupMainBox();
-		this.mainBox.add(centerBox);
-		this.mainBox.add(Box.createVerticalStrut(40));
-		this.add(mainBox);
+		this.setupPanel();
 	}
 
-	public void setupMainBox(){
+	public void setupPanel(){
 		createPanelWelcome();
 		createScrollPane();
-		createTipsVideoPane();
+		createTipsPanel();
 	}
 
 	public void createPanelWelcome () {
 		JPanel panelWelcome = new JPanel(new GridLayout(0,1));
 		JLabel lblWelcome = new JLabel("Welcome to CLIMBCOL");
-		this.mainBox.add(panelWelcome);
-		this.mainBox.add(Box.createVerticalStrut(20));
 		lblWelcome.setFont(new Font("Tahoma",Font.PLAIN,35));
 		JPanel jp1 = new JPanel();
 		jp1.add(lblWelcome);
@@ -62,13 +62,23 @@ public class UIWelcome extends JPanel{
 		jp1.add(lblDescription);
 		panelWelcome.add(jp1);
 
-		/*final JComboBox<String> cbox = new JComboBox<String>();// agregar listener
-		ArrayList<String> rutes = ClimbColManager.getAllRutes();
-		for(String s : rutes)
-			cbox.addItem(s);
-		cbox.setEditable(true);
-		panelWelcome.add(cbox);*/
+		/*DefaultComboBoxModel<Ruta> model = new DefaultComboBoxModel<>();
+		ArrayList<Ruta> rutes = ClimbColManager.getAllRutes();
+		for(Ruta r : rutes) {
+			model.addElement(r);
+		}
+		JComboBox <Ruta> cBoxRutes = new JComboBox <Ruta> (model);
+		cBoxRutes.setRenderer(new Renderer());
+		cBoxRutes.addActionListener(new ActionListener () {
+			public void actionPerformed(ActionEvent e) {
+				JComboBox <Ruta> cb = (JComboBox<Ruta>) e.getSource();
+				uiMain.showPanel(UIRute.createUIRute((Ruta) cb.getSelectedItem(), uiMain));
+			}			
+		});
+		panelWelcome.add(cBoxRutes);*/
 
+		this.constraints.gridwidth = 2;	
+		this.addGB(panelWelcome,0,0);
 
 	}
 
@@ -78,38 +88,37 @@ public class UIWelcome extends JPanel{
 			model.addElement(park);
 		}
 		JList <Parque> listParks = new JList <Parque> (model);
-		listParks.setCellRenderer(new Renderer());//ojo
+		listParks.setCellRenderer(new Renderer());
 		JScrollPane scrollPaneParks = new JScrollPane(listParks);
-
-
-		centerBox.add(Box.createHorizontalStrut(40));
-		centerBox.add(scrollPaneParks);
-		centerBox.add(Box.createHorizontalStrut(30));
+		scrollPaneParks.setMinimumSize(new Dimension(400,500));
 
 		listParks.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent arg0) {
-				Parque park = ClimbColManager.getPark((listParks.getSelectedValue().getName()));
-				uiMain.showPanel(UIPark.createUIPark(park, uiMain));	
+				uiMain.showPanel(UIPark.createUIPark(listParks.getSelectedValue(), uiMain));	
 			}
 		});
+
+		this.constraints.gridwidth = 1;
+		this.constraints.gridheight = 1;
+		this.constraints.insets = new Insets(10,20,0,20);
+		this.addGB(scrollPaneParks, 0, 1);
 	}
 
+	public void createTipsPanel() {
+		JPanel tips = new JPanel(new GridLayout(0,1));
 
-	public void createTipsVideoPane() {
-		JPanel tipsVideo = new JPanel();
-		tipsVideo.setLayout(new GridLayout(0,1));
+		JLabel labelTipsImage1 = new JLabel(new ImageIcon("images/2.jpg"));
+		tips.add(labelTipsImage1);
 
-		ImageIcon Tips= new ImageIcon("images/2.jpg");
-		JLabel labelTips = new JLabel(Tips);
-		labelTips.setVisible(true);
-		labelTips.setSize(new Dimension(2,2));
-		tipsVideo.add(labelTips);
+		JLabel labelTipsImage2 = new JLabel(new ImageIcon("images/3.jpg"));
+		tips.add(labelTipsImage2);
 
-		ImageIcon Video= new ImageIcon("images/3.jpg");
-		JLabel labelVideo = new JLabel(Video);
-		labelVideo.setVisible(true);
-		tipsVideo.add(labelVideo);
-		centerBox.add(tipsVideo);
-		centerBox.add(Box.createHorizontalStrut(40));
+		this.addGB(tips, 1, 1);
+	}
+
+	private void addGB(Component comp, int x, int y) {
+		this.constraints.gridx = x;
+		this.constraints.gridy = y;
+		this.add(comp, this.constraints);
 	}
 }
