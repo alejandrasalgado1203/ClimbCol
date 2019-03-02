3e897b3b95ed8df79d8c5f827e89a5f055473670
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-03-02 17:13:11 -0500

    Update FileNameConstants.java

--- ClimbCol/src/business/FileNameConstants.java
+++ ClimbCol/src/business/FileNameConstants.java
@@ -4,5 +4,6 @@ public interface FileNameConstants {
 
 	public static final String PARKS_FILE = "parks.ser";
 	public static final String CLIMBERS_FILE = "climbers.ser";
+	public static final String DEFAULT_USER_IMAGE = "images/3.jpg";
 
 }
