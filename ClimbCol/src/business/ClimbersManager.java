e587a9ef14e936b6a3ddb2c3a2e3218a128a347f
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-03-02 11:57:16 -0500

    Update ClimbersManager.java

--- ClimbCol/src/business/ClimbersManager.java
+++ ClimbCol/src/business/ClimbersManager.java
@@ -7,14 +7,7 @@ import data.Escalador;
 public class ClimbersManager {
 
 	private static TreeMap<String, Escalador> escaladores;
-
-	public static TreeMap<String, Escalador> getEscaladores() {
-		return escaladores;
-	}
-
-	public static void setEscaladores(TreeMap<String, Escalador> escaladores) {
-		ClimbersManager.escaladores = escaladores;
-	}
+	private static Escalador currentUser;
 
 	public static void creatUser(String text, char[] password) {
 		Escalador climber = new Escalador(text,password);
@@ -30,6 +23,35 @@ public class ClimbersManager {
 			if(c.getName().equals(text))return false;
 		}
 		return true;
+	}
+
+	public static boolean doLogin(String text, char[] password) {
+		boolean succesful = false;
+		Escalador user = escaladores.get(new String (password));
+		if(user!=null && user.getName().equals(text)) {
+			succesful = true;
+			currentUser = user;
+		}
+		return succesful;
+	}
+
+
+
+
+	public static TreeMap<String, Escalador> getEscaladores() {
+		return escaladores;
+	}
+
+	public static void setEscaladores(TreeMap<String, Escalador> escaladores) {
+		ClimbersManager.escaladores = escaladores;
+	}
+
+	public static Escalador getCurrentUser() {
+		return currentUser;
+	}
+
+	public static void setCurrentUser(Escalador currentUser) {
+		ClimbersManager.currentUser = currentUser;
 	}
 
 }
