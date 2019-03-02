531efb90c36d3b8db784b77328f14446a71c1419
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-03-02 17:10:16 -0500

    Update UIUser.java

--- ClimbCol/src/ui/UIUser.java
+++ ClimbCol/src/ui/UIUser.java
@@ -2,9 +2,12 @@
 package ui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,10 +24,11 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
@@ -35,103 +39,129 @@ import data.Escalador;
 import data.Ruta;
 
 public class UIUser extends JFrame {
-	private JPanel panelTittle = new JPanel();
-	private JPanel panelAll = new JPanel(new GridLayout(1,2));
-	private JPanel panelInfo = new JPanel(new GridLayout(0,1));
+
 	private Escalador climber;
 	private UIMain uiMain;
-	//private static JFrame signInFrame;
+	private JPanel userPanel;
+	private JPanel editPanel;
+	private GridBagConstraints constraints = new GridBagConstraints();
 
-	public UIUser(Escalador climber) {
-		super("info User");
-		this.climber = climber;
-		setupMainPanel();
-		this.panelAll.add(panelInfo);
-		this.add(panelAll, BorderLayout.CENTER);
+	public UIUser(UIMain main) {
+		super("User's info");
+		this.climber = ClimbersManager.getCurrentUser();
+		this.uiMain = main;
+		createMenu();
+		showUserPanel();
 		this.setResizable(true);
 		this.pack();
 		this.setVisible(true);
 	}
-	public   void setupMainPanel() {
-		createTittle();
-		createImage();
-		createTextField();
-		creatSpinner();
-		createButtons();
+
+	private void createMenu() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenuItem menuItemEdit = new JMenuItem("Edit");
+		menuBar.add(menuItemEdit);
+		menuItemEdit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showEditPanel();
+			}
+		});
+		this.setJMenuBar(menuBar);
 	}
 
-	public void createTittle() {
-		JLabel lblWelcomeUser = new JLabel(this.climber.getName());
-		lblWelcomeUser.setFont(new Font("Tahoma",Font.PLAIN,35));
-		panelTittle.add(lblWelcomeUser);
-		this.add(panelTittle,BorderLayout.NORTH);
+	private void showUserPanel() {
+		this.userPanel = new JPanel(new GridBagLayout());
+		this.constraints.insets = new Insets(10,10,10,10);
+		createTittle();
+		createImage();
+		createInfo();
+		createButtons();
+		this.add(userPanel);
+		this.pack();
+	}
+
+	private void createTittle() {
+		JLabel lblTittle = new JLabel(this.climber.getName());
+		lblTittle.setFont(new Font("Tahoma",Font.PLAIN,35));
+		constraints.gridwidth = 2;
+		constraints.anchor = GridBagConstraints.CENTER;
+		this.addGB(userPanel,lblTittle, 0, 0);
 	}
 
 	private void createImage() {
-		JPanel imageUser= new JPanel();
 		ImageIcon user= new ImageIcon(this.climber.getDireccionImagen());
 		JLabel labelImage = new JLabel(user);
-		labelImage.setVisible(true);
-		imageUser.add(labelImage);
-		this.panelAll.add(imageUser);
+		constraints.gridheight = 2;
+		constraints.gridwidth = 1;
+		constraints.anchor = GridBagConstraints.CENTER;
+		this.addGB(userPanel,labelImage, 0, 1);
 	}
 
-	public   void createTextField() { 
-		JTextField textfield = new JTextField(30);
-		textfield.setEditable(false);
-		textfield.setText(this.climber.toString());
-		this.panelInfo.add(textfield);
+	private   void createInfo() { 
+		JPanel infoPanel = new JPanel(new GridLayout(0,1));
+
+		JLabel lbl = new JLabel("Birthdate: " + climber.getFechaDeNacimiento());
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,20));
+		infoPanel.add(lbl);
+		lbl = new JLabel("Achieved routes: " + climber.getLogradas());
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,20));
+		infoPanel.add(lbl);
+		lbl = new JLabel("Favorite climbing: " + climber.getEscaladaFavorita());
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,20));
+		infoPanel.add(lbl);
+		lbl = new JLabel("Maximum difficulty achieved: " + climber.getMaximaDificultadLograda());
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,20));
+		infoPanel.add(lbl);
+
+		constraints.gridheight = 1;
+		constraints.gridwidth = 1;
+		constraints.anchor = GridBagConstraints.CENTER;
+		this.addGB(userPanel, infoPanel, 1, 1);
 	}
 
-	private void creatSpinner() {
-		JSpinner spinnLogradas = new JSpinner();
+	private void createButtons() {
+		JPanel btnsPanel = new JPanel(); 
 
-
-		this.panelInfo.add(spinnLogradas);
-	}
-
-	public void createButtons() {
-
-		JPanel btns = new JPanel(); 
 		JRadioButton buttonGoals= new JRadioButton("Grupo de Retos");
-		buttonGoals.setForeground(Color.BLACK);
-		buttonGoals.setBackground(Color.WHITE);
+		buttonGoals.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				setupPanelRutes(climber.getRetos().values());
+			}
+		});
 
 		JRadioButton buttonFavorites = new JRadioButton("Favoritos");
-		buttonFavorites.setForeground(Color.BLACK);
-		buttonFavorites.setBackground(Color.WHITE);
+		buttonFavorites.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				setupPanelRutes(climber.getFavoritos().values());
+			}
+		});
 
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(buttonFavorites);
 		bg.add(buttonGoals);
 
-		btns.add(buttonGoals);
-		btns.add(buttonFavorites);
-		buttonGoals.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				setuoPanelRutes(climber.getRetos().values());
-			}
+		btnsPanel.add(buttonGoals);
+		btnsPanel.add(buttonFavorites);
 
-		});
-
-		buttonFavorites.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				setuoPanelRutes(climber.getFavoritos().values());
-			}
-		});
-		this.panelInfo.add(btns);
+		constraints.gridheight = 1;
+		constraints.gridwidth = 1;
+		constraints.anchor = GridBagConstraints.CENTER;
+		this.addGB(userPanel, btnsPanel, 1, 2);
 	}
 
-	public   void setuoPanelRutes(Collection<Ruta> collection) {//modificar
+	private void setupPanelRutes(Collection<Ruta> collection) {
 		DefaultListModel<Ruta> model = new DefaultListModel<>();
 		for (Ruta r: collection) {
 			model.addElement(r);
 		}
 		JList <Ruta> listRutes = new JList <Ruta> (model);
 		listRutes.setCellRenderer(new Renderer());
-		JScrollPane scrollPaneRutes = new JScrollPane(listRutes);
+		JScrollPane scrollPaneRoutes = new JScrollPane(listRutes);
 
-		showPanelRutes(scrollPaneRutes);
+		constraints.gridheight = 1;
+		constraints.gridwidth = 1;
+		constraints.anchor = GridBagConstraints.CENTER;
+		this.addGB(userPanel, scrollPaneRoutes, 1, 3);
 
 		listRutes.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent arg0) {
@@ -139,22 +169,96 @@ public class UIUser extends JFrame {
 				uiMain.showPanel(UIRute.createUIRute(rute, uiMain));	
 			}
 		});
-
 	}
 
-	private void showPanelRutes(JScrollPane scrollPaneParks) {
-		if (this.panelInfo.getComponentCount()==4)
-			this.panelInfo.remove(3);
-		this.panelInfo.add(scrollPaneParks);
+	private void showEditPanel() {
+		this.editPanel = new JPanel(new GridBagLayout());
+		createEditImage();
+		creatEditInfo();
+		createEditButtons();
+		this.remove(userPanel);
+		this.add(editPanel, BorderLayout.CENTER);
+		this.pack();
+	}
+
+	private void createEditImage() {
+		ImageIcon user= new ImageIcon(this.climber.getDireccionImagen());
+		JLabel labelImage = new JLabel(user);
+		constraints.gridheight = 2;
+		constraints.gridwidth = 1;
+		constraints.anchor = GridBagConstraints.CENTER;
+		this.addGB(editPanel,labelImage, 0, 0);
+	}
+
+	private void creatEditInfo() {
+		JPanel editInfoPanel = new JPanel(new GridLayout(0,1));
+
+		JLabel lbl = new JLabel("Birthdate: ");
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,15));
+		editInfoPanel.add(lbl);
+		JTextField txtBirthdate = new JTextField();
+		editInfoPanel.add(txtBirthdate);
+		lbl = new JLabel("Achieved routes: ");
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,15));
+		editInfoPanel.add(lbl);
+		JTextField txtAchievedRoutes = new JTextField();
+		editInfoPanel.add(txtAchievedRoutes);
+		lbl = new JLabel("Favorite climbing: ");
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,15));
+		editInfoPanel.add(lbl);
+		JTextField txtFavoriteClimbing = new JTextField();
+		editInfoPanel.add(txtFavoriteClimbing);
+		lbl = new JLabel("Maximum difficulty achieved: ");
+		lbl.setFont(new Font("Tahoma",Font.PLAIN,15));
+		editInfoPanel.add(lbl);
+		JTextField txtMaxDifficulty = new JTextField();
+		editInfoPanel.add(txtMaxDifficulty);
+
+		constraints.gridheight = 1;
+		constraints.gridwidth = 1;
+		constraints.anchor = GridBagConstraints.CENTER;
+		this.addGB(editPanel, editInfoPanel, 1, 0);
+	}
+
+	private void createEditButtons() {
+		JButton btnEditImage = new JButton("Edit Image");
+		btnEditImage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		constraints.gridheight = 1;
+		constraints.gridwidth = 1;
+		this.addGB(editPanel, btnEditImage, 0, 2);
+
+		JButton btnSave = new JButton("save");
+		btnSave.addActionListener(new ActionListener () {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		this.addGB(editPanel, btnSave, 0, 3);
+
+		JButton btnCancel = new JButton("cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				remove(editPanel);
+				showUserPanel();
+			}
+		});
+		this.addGB(editPanel, btnCancel, 1, 3);
+	}
+
+	private void addGB(JPanel jp,Component comp, int x, int y) {
+		constraints.gridx = x;
+		constraints.gridy = y;
+		jp.add(comp, constraints);
 	}
 
 
 	//Sign in
-
 	public static void showSignInFrame() {
 		JFrame frame = new JFrame ();
-		JLabel lblTittle = new JLabel ("Sign in");
-		lblTittle.setFont(new Font("Tahoma",Font.PLAIN,25));
 
 		JLabel nameLabel= new JLabel("Name: ");
 		JTextField nameField =  new JTextField(25);
@@ -215,16 +319,13 @@ public class UIUser extends JFrame {
 
 		Object [] message = new Object[] {nameLabel,nameField,passwordLabel,passwordField};
 		Object[] options = new Object[] {btnSignIn, btnCancel};
-		JOptionPane.showOptionDialog(frame, message, "Sign in", JOptionPane.CANCEL_OPTION,
+		JOptionPane.showOptionDialog(frame, message, "Sign in", 0,
 				JOptionPane.QUESTION_MESSAGE, null, options, btnSignIn);
 	}
 
 	//login
 	public static void showLoginFrame() {
 		JFrame frame = new JFrame();
-
-		JLabel lblTittle = new JLabel ("Login");
-		lblTittle.setFont(new Font("Tahoma",Font.PLAIN,25));
 
 		JLabel nameLabel= new JLabel("Name: ");
 		JTextField nameField =  new JTextField(25);
@@ -260,7 +361,7 @@ public class UIUser extends JFrame {
 
 		Object [] message = new Object[] {nameLabel,nameField,passwordLabel,passwordField};
 		Object[] options = new Object[] {btnLogin, btnCancel};
-		JOptionPane.showOptionDialog(frame, message, "Sign in", JOptionPane.CANCEL_OPTION,
+		JOptionPane.showOptionDialog(frame, message, "Sign in", 0,
 				JOptionPane.QUESTION_MESSAGE, null, options, btnLogin);
 	}
 }
