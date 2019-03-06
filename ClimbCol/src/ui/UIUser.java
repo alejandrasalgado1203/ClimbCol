c9c1559ebb1c3bba2bb973aada25fcc1f61ddd13
dfigueroa11 <47487793+dfigueroa11@users.noreply.github.com>
2019-03-05 21:41:08 -0500

    Update UIUser.java

--- ClimbCol/src/ui/UIUser.java
+++ ClimbCol/src/ui/UIUser.java
@@ -10,6 +10,8 @@ import java.awt.Insets;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.NumberFormat;
@@ -63,6 +65,13 @@ public class UIUser extends JFrame {
 		this.uiMain = main;
 		createMenu();
 		showUserPanel();
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent windowEvent) {
+				uiMain.createMenuBar();
+				setVisible(false);
+			}
+		});
 		this.setResizable(true);
 		this.pack();
 		this.setVisible(true);
